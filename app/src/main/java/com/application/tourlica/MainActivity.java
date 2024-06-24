9ac7064023f268489b231d0aa72f60aa2fee563c
munbyeongcheon <munbc8308@gmail.com>
2024-06-24 17:05:34 +0900

    [24.6.12] munbc
    kakao map test

--- app/src/main/java/com/application/tourlica/MainActivity.java
+++ app/src/main/java/com/application/tourlica/MainActivity.java
@@ -4,25 +4,13 @@ import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
-import android.view.View;
 
+import androidx.activity.ComponentActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.application.tourlica.databinding.ActivityMainBinding;
+
 import com.kakao.vectormap.KakaoMapSdk;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -35,10 +23,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+public class MainActivity extends ComponentActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
@@ -53,11 +38,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         KakaoMapSdk.init(this, "d676018bf28505ed778825aeca90dfd6");
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = location -> {
@@ -84,59 +64,8 @@ public class MainActivity extends AppCompatActivity {
                 2000, // 몇미터 이동에 한번씩 찾을까
                 locationListener);// 어떤 코드를 실행 시킬것인지.
 
-
-
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(() -> {
-                    try {
-                        sign_in();
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 
     private String sign_in() throws JSONException {
         final MediaType JSON = MediaType.get("application/json");
